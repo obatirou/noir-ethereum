7bad13022ecb3c234742eb53983eaf2db253609e
Vedant Chainani <vedantchainani1084@gmail.com>
2025-04-15 00:05:08 +0530

    chore: add docs for accounts

--- index.ts
+++ index.ts
@@ -1,7 +1,50 @@
-import { Trie } from '@ethereumjs/trie';
-import { toBytes } from '@ethereumjs/util';
-import { http, createPublicClient, fromRlp, keccak256, toHex } from 'viem';
+import {
+  http,
+  createPublicClient,
+  hexToBytes,
+  keccak256,
+  toHex,
+  toRlp,
+} from 'viem';
 import { mainnet } from 'viem/chains';
+
+const parseArray = (data: number[] | `0x${string}` | Uint8Array) => {
+  const arr = Array.isArray(data)
+    ? data
+    : // biome-ignore lint/nursery/noNestedTernary: <explanation>
+      data instanceof Uint8Array
+      ? Array.from(data)
+      : Array.from(hexToBytes(data));
+  return `[${arr.join(', ')}]`;
+};
+
+const leftPad = (
+  data: number[] | `0x${string}` | Uint8Array,
+  finalLength: number
+) => {
+  const arr = Array.isArray(data)
+    ? data
+    : // biome-ignore lint/nursery/noNestedTernary: <explanation>
+      data instanceof Uint8Array
+      ? Array.from(data)
+      : Array.from(hexToBytes(data));
+  const zeroToAdd = finalLength - arr.length;
+  return [...new Array<number>(zeroToAdd).fill(0), ...arr];
+};
+
+const rightPad = (
+  data: number[] | `0x${string}` | Uint8Array,
+  finalLength: number
+) => {
+  const arr = Array.isArray(data)
+    ? data
+    : // biome-ignore lint/nursery/noNestedTernary: <explanation>
+      data instanceof Uint8Array
+      ? Array.from(data)
+      : Array.from(hexToBytes(data));
+  const zeroToAdd = finalLength - arr.length;
+  return [...arr, ...new Array<number>(zeroToAdd).fill(0)];
+};
 
 export const publicClient = createPublicClient({
   chain: mainnet,
@@ -9,43 +52,49 @@ export const publicClient = createPublicClient({
 });
 
 const block = await publicClient.getBlock();
-const stateRoot = block.stateRoot;
+console.log(block);
+
+const address = '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045';
 
 const p = await publicClient.getProof({
-  address: '0x4200000000000000000000000000000000000016',
-  storageKeys: [
-    '0x4a932049252365b3eedbc5190e18949f2ec11f39d3bef2d259764799a1b27d99',
-  ],
+  address,
+  storageKeys: [],
   blockNumber: block.number,
 });
 
-console.log(p);
-console.log('\n\n');
-
-const proof = {
-  accountProof: p.accountProof.map((x) => toBytes(x)),
+const account = {
+  address: parseArray(p.address),
+  balance: toHex(p.balance),
+  codeHash: parseArray(p.codeHash),
+  nonce: toHex(p.nonce),
+  storageHash: parseArray(p.storageHash),
 };
 
-// Initialize the state trie with the stateRoot
-const stateTrie = await Trie.createFromProof(proof.accountProof);
+const leafNode = p.accountProof.at(-1) ?? '0x00';
+const nodes = p.accountProof.slice(0, -1);
 
-// Convert the account address to a buffer
-const addressHash = toBytes(keccak256(p.address));
+const proof = {
+  nodes: nodes.map((val) => {
+    const parsed = parseArray(rightPad(val, 532));
+    return parsed;
+  }),
+  leaf: parseArray(rightPad(leafNode, 148)),
+  depth: p.accountProof.length,
+};
 
-// Verify the account proof
-const accountValue = await stateTrie.verifyProof(
-  stateTrie.root(),
-  addressHash,
-  proof.accountProof
-);
+const accountProof = {
+  key: parseArray(leftPad(keccak256(p.address, 'bytes'), 66)),
+  value: parseArray(
+    leftPad(
+      toRlp([toHex(p.nonce), toHex(p.balance), p.storageHash, p.codeHash]),
+      110
+    )
+  ),
+  proof,
+};
 
-if (!accountValue) {
-  throw new Error('Account value not found');
-}
-
-const accountRlp = toHex(accountValue);
-
-const extractedStorageRoot = fromRlp(accountRlp, 'hex')[2];
-
-console.log('Extracted Storage Root: ', extractedStorageRoot);
-console.log('Expected Storage Root: ', p.storageHash);
+// console.log({
+//   account,
+//   accountProof,
+//   stateRoot: parseArray(block.stateRoot),
+// });
