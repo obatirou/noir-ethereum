11e80404582c413619b539892c39939fdc1954f0
Oba <obatirou@gmail.com>
2025-06-03 12:03:03 +0200

    add getUSDCBalanceStorageSlot

--- src/storage.ts
+++ src/storage.ts
@@ -1,22 +1,46 @@
-import { Trie } from '@ethereumjs/trie';
-import { FixedSizeArray, U8, U64 } from '@zkpersona/noir-helpers';
+import { Trie } from "@ethereumjs/trie";
+import { FixedSizeArray, U8, U64 } from "@zkpersona/noir-helpers";
 import {
   type GetBlockParameters,
   type Hex,
   type PublicClient,
   hexToBytes,
   keccak256,
-} from 'viem';
-import { leftPad, parseBytes32, rightPad } from './helpers';
+  type Address,
+  encodeAbiParameters,
+} from "viem";
+import { leftPad, parseBytes32, rightPad } from "./helpers";
 
 export type GetStorageProofOpts = {
   address: Hex;
   slot: Hex;
 } & GetBlockParameters;
 
+// mapping(address => uint256) internal balanceAndBlacklistStates;
+export const getUSDCBalanceStorageSlot = (address: Address) => {
+  const slot = 9n;
+
+  const finalSlot = keccak256(
+    encodeAbiParameters(
+      [
+        {
+          name: "address",
+          type: "address",
+        },
+        {
+          name: "slot",
+          type: "uint256",
+        },
+      ],
+      [address, slot],
+    ),
+  );
+  return finalSlot;
+};
+
 export const getStorageProof = async <T extends PublicClient>(
   publicClient: T,
-  opts: GetStorageProofOpts
+  opts: GetStorageProofOpts,
 ) => {
   const { address, slot, ...getBlockOpts } = opts;
   const block = await publicClient.getBlock(getBlockOpts);
@@ -27,25 +51,28 @@ export const getStorageProof = async <T extends PublicClient>(
     blockNumber: block.number ?? undefined,
   });
 
+  console.log(slot);
+  console.log(keccak256(slot, "bytes"));
+
   const storageProof = res.storageProof[0]?.proof;
 
   if (!storageProof) {
-    throw new Error('Storage Proof not found.');
+    throw new Error("Storage Proof not found.");
   }
 
   const storageTrie = new Trie({ root: hexToBytes(res.storageHash) });
   const storageProofBuffers = storageProof.map((p) => hexToBytes(p));
   const storageValueRlp = await storageTrie.verifyProof(
     storageTrie.root(),
-    keccak256(slot, 'bytes'),
-    storageProofBuffers
+    keccak256(slot, "bytes"),
+    storageProofBuffers,
   );
 
   if (!storageValueRlp) {
-    throw new Error('Storage Proof verification failed');
+    throw new Error("Storage Proof verification failed");
   }
 
-  const leafNode = storageProof.at(-1) ?? '0x0';
+  const leafNode = storageProof.at(-1) ?? "0x0";
   const nodes = storageProof.slice(0, -1);
 
   const nodesData = nodes.map((val) => {
@@ -55,7 +82,7 @@ export const getStorageProof = async <T extends PublicClient>(
 
   if (nodesData.length > 6) {
     throw new Error(
-      'Storage Proof can have at most 6 nodes excluding the leaf node'
+      "Storage Proof can have at most 6 nodes excluding the leaf node",
     );
   }
 
@@ -67,7 +94,7 @@ export const getStorageProof = async <T extends PublicClient>(
     nodes: new FixedSizeArray(6, nodesData),
     leaf: new FixedSizeArray(
       69,
-      rightPad(leafNode, 69).map((x) => new U8(x))
+      rightPad(leafNode, 69).map((x) => new U8(x)),
     ),
     depth: new U64(storageProof.length),
   };
@@ -75,11 +102,11 @@ export const getStorageProof = async <T extends PublicClient>(
   const storageRoot = parseBytes32(res.storageHash);
   const key = new FixedSizeArray(
     66,
-    leftPad(keccak256(slot, 'bytes'), 66).map((x) => new U8(x))
+    leftPad(keccak256(slot, "bytes"), 66).map((x) => new U8(x)),
   );
   const value = new FixedSizeArray(
     33,
-    leftPad(storageValueRlp, 33).map((x) => new U8(x))
+    leftPad(storageValueRlp, 33).map((x) => new U8(x)),
   );
 
   const proofInput = {
